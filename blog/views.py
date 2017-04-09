2c4f4439532abd48f28ff622df196128688c1e04
Max J. Schridde <maxschridde@berkeley.edu>
2017-04-09 12:50:26 -0700

    Designed layout for comments part of blog posts.

--- blog/views.py
+++ blog/views.py
@@ -8,6 +8,8 @@ from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from utils import gravatar, fetch_prev_next, create_blog_months
 
+months = {'1':'January','2':'February','3':'March','4':'April','5':'May','6':'June','7':'July','8':'August','9':'September','10':'Octoboer','11':'November','12':'December'}
+
 def blogfeed(request):
 	blog_posts = BlogPost.objects.order_by('-pub_date')
 	auth_post = [(post, User.objects.get(username=post.author), gravatar(User.objects.get(username=post.author).email, 100)) for post in blog_posts]
@@ -29,7 +31,7 @@ def blogfeed_month(request, year, month):
 	filtered = [(post, User.objects.get(username=post.author), gravatar(User.objects.get(username=post.author).email, 100)) for post in posts]
 	context = {
 		'blog_posts_list': filtered,
-		'month_year': month_year,
+		'month_year': create_blog_months(),
 		'year': year,
 		'month': months[str(month)],
 	}
@@ -46,10 +48,14 @@ def detail(request, entry_id):
 	context = {'entry': bp,
 		'prev_next': p_n
 	}
-	context['author_img'] = gravatar(User.objects.get(username=bp.author).email, 50, "img-circle")
+	context['author_img'] = gravatar(User.objects.get(username=bp.author).email, 80, "img-circle")
 	c = bp.comment_set.order_by('-pub_date')
-	comments = [(comment, comment.user.username, gravatar(comment.user.email, 50, "img-circle comment-pic")) for comment in c]
+	comments = [{'comment':comment, 'username':comment.user.username, 'img':gravatar(comment.user.email, 50, "img-circle comment-pic")} for comment in c]
 	context['comments'] = comments
+	if len(comments) != 1:
+		context['comment_header'] = "%s Comments" % str(len(comments))
+	else:
+		context['comment_header'] = "1 Comment" 
 	if request.user.is_authenticated:
 		template="blog/blogdetailloggedin.html"
 		context['img'] = gravatar(User.objects.get(username=request.user).email)
