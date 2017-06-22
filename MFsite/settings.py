f34ace7b8429e522dc6b8c8448084a27a2ecb585
Max J. Schridde <maxschridde@berkeley.edu>
2017-06-22 10:42:18 -0700

    collected static files.

--- MFsite/settings.py
+++ MFsite/settings.py
@@ -156,9 +156,12 @@ STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-    'MFsite/masterfaster/static',
-    'MFsite/blog/static',
-    'MFsite/sales/static',
+    # 'MFsite/masterfaster/static',
+    # 'MFsite/blog/static',
+    # 'MFsite/sales/static',
+    'masterfaster/static',
+    'blog/static',
+    'sales/static',
 )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
