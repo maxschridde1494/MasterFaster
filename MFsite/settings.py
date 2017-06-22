89137af2f07e18e5b1f247be807511609131f30b
Max J. Schridde <maxschridde@berkeley.edu>
2017-06-22 11:35:20 -0700

    ..

--- MFsite/settings.py
+++ MFsite/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print('BASE DIR \n \n \n')
 print(BASE_DIR)
 
 
@@ -157,9 +158,11 @@ STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-    'MFsite/masterfaster/static',
-    'MFsite/blog/static',
-    'MFsite/sales/static',
+    os.path.join(BASE_DIR, '../masterfaster/static'),
+    os.path.join(BASE_DIR, '../blog/static'),
+    # 'MFsite/masterfaster/static',
+    # 'MFsite/blog/static',
+    # 'MFsite/sales/static',
 )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
