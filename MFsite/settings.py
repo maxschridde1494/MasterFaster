70d23a898e28197a594198af8fb887f7d1000cce
Max J. Schridde <maxschridde@berkeley.edu>
2017-06-22 11:06:53 -0700

    ..

--- MFsite/settings.py
+++ MFsite/settings.py
@@ -151,7 +151,7 @@ USE_TZ = True
 
 
 STATIC_ROOT =  os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
