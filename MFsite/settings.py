ece056c49108605fc255c92dc57eed4df5dcaec5
Max J. Schridde <maxschridde@berkeley.edu>
2017-06-22 11:16:39 -0700

    ..

--- MFsite/settings.py
+++ MFsite/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print(BASE_DIR)
 
 
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
@@ -151,17 +152,17 @@ USE_TZ = True
 
 
 STATIC_ROOT =  os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-    # 'MFsite/masterfaster/static',
-    # 'MFsite/blog/static',
-    # 'MFsite/sales/static',
-    'masterfaster/static',
-    'blog/static',
-    'sales/static',
+    '../MFsite/masterfaster/static',
+    '../MFsite/blog/static',
+    '../MFsite/sales/static',
+    # 'masterfaster/static',
+    # 'blog/static',
+    # 'sales/static',
 )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
