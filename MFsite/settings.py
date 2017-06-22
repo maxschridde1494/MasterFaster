97c9581df50aa14caf2cf864b9c0eaf2cfada4b4
Max J. Schridde <maxschridde@berkeley.edu>
2017-06-22 12:04:51 -0700

    ..

--- MFsite/settings.py
+++ MFsite/settings.py
@@ -14,9 +14,6 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-print('BASE DIR \n \n \n')
-print(BASE_DIR)
-
 
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
