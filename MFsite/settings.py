93af520aab66272ca8dac671be849899f95bc025
Max J. Schridde <maxschridde@berkeley.edu>
2017-07-11 18:37:36 -0700

    still messing with settings.py

--- MFsite/settings.py
+++ MFsite/settings.py
@@ -2,6 +2,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print(BASE_DIR + '/../')
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
